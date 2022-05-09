edc705cc65bf1ae99968cad8e190a92cbeccf705
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-05-09 10:37:40 +0530

    Add files via upload

---
+++ 264_bp1/264_bp1.ino
@@ -0,0 +1,19 @@
+void setup(){
+  
+  //  initialize the serial communication
+}
+
+void loop()
+{
+  
+  //  read the state of pin 2
+  
+  //  convert it to string format
+  
+  //  send that data towards arduino 2
+  
+  //  wait for 500 ms
+}
+
+
+
---
+++ 264_bp2/264_bp2.ino
@@ -0,0 +1,22 @@
+void setup()
+{
+  //  initialize the serial communication
+  
+  //  set the timeout
+  
+  // configure pin 3 as ouptut
+}
+
+void loop()
+{
+  
+  //  check if data is available
+  
+  //  read it in a string
+  
+  //  convert in to an integer
+  
+  //  check if data received is 1, turn on the LED
+  
+  //  else turn off the LED
+}
